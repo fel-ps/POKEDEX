c4028566f3d9ce6596d432800d81a883c9aacb37
Felipe Augusto Araújo da Cunha <araujofelipe54@gmail.com>
2024-06-13 19:05:58 -0300

    building
    adiantamento do pequeno projeto até aula 13, no qual foi construído as cards e a lista com filtro

--- pokevite/src/views/HomeView.vue
+++ pokevite/src/views/HomeView.vue
@@ -1,9 +1,79 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+  import { onMounted, reactive, ref, computed } from 'vue';
+  import ListPokemons from "../components/ListPokemons.vue"
+
+
+  let urlBaseSvg = ref("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/")
+  let pokemons = reactive(ref());
+  let searchPokemonField = ref("")
+
+  onMounted (() =>{
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+    .then(res => res.json())
+    .then(res => pokemons.value = res.results);
+
+  })
+
+  const pokemonsFiltered = computed(()=>{
+    if(pokemons.value && searchPokemonField.value){
+      return pokemons.value.filter(pokemon=>
+        pokemon.name.toLowerCase().includes(searchPokemonField.value.toLowerCase())
+      )
+    }
+    return pokemons.value;
+  })
+
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="container">
+
+
+      <div class="row mt-4">
+        <!-- <div class="col-sm-12 col-md-6">
+          <div class="card" style="width: 18rem;">
+            <img src="https://static.miraheze.org/greatcharacterswiki/thumb/4/47/OfficialMewtwoDesign.png/800px-OfficialMewtwoDesign.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div> -->
+
+        <div class="col-sm-12 col-md-6">
+          <div class="card">
+            <div class="card-body row">
+
+              <div class="mb-3">
+                <label 
+                hidden 
+                for="exampleFormControlInput1" 
+                class="form-label">
+                Pesquisar...
+                </label>
+
+                <input 
+                v-model="searchPokemonField"
+                type="text" 
+                class="form-control" 
+                id="searchPokemonField" 
+                placeholder="Pesquisar...">
+              </div>
+
+              <ListPokemons
+              v-for="pokemon in pokemonsFiltered"
+              :key="pokemon.name"
+              :name="pokemon.name"
+              :urlBaseSvg="urlBaseSvg + pokemon.url.split('/')[6] +'.svg'"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
   </main>
 </template>
