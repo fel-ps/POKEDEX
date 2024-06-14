1c2cd1050d2bf6d9a6ae146897cff0c81f82b5af
Felipe Augusto Araújo da Cunha <araujofelipe54@gmail.com>
2024-06-14 17:10:06 -0300

    pwa
    preparando para subi-lo pro netlify

--- pokevite/vite.config.js
+++ pokevite/vite.config.js
@@ -3,10 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VitePWA({ registerType: 'autoUpdate' }),
     vue(),
     VueDevTools(),
   ],
